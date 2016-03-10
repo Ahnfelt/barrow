ac0e4a7f44c57e9a1ffe8c661b8500c894e0d96a
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2016-03-10 22:40:32 +0100

    ...

---
+++ client/react.ts
@@ -0,0 +1,6 @@
+/// <reference path="../DefinitelyTyped/react/react.d.ts" />
+/// <reference path="../DefinitelyTyped/react/react-dom.d.ts" />
+
+import React = require('react');
+import ReactDOM = require('react-dom');
+
